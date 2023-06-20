a1c377ab05bcab33eb952aaec5e09b0fa5a57a92
kchalkowski <kzc0061@auburn.edu>
2023-06-20 10:42:28 -0600

    updated function sourcer with new funcs

--- Scripts/ASFFunctionSourcer.R
+++ Scripts/ASFFunctionSourcer.R
@@ -1,10 +1,14 @@
 #R functions
 source(paste(getwd(), "/Scripts/areaOfinfection.R", sep = ''))
 source(paste(getwd(), "/Scripts/CullingOneRun.R", sep = ''))
+source(paste(getwd(), "/Scripts/FirstDetect.R", sep = ''))
+source(paste(getwd(), "/Scripts/GetOutputs.R", sep = ''))
 source(paste(getwd(), "/Scripts/GenerateFakeStateData.R", sep = ''))
+source(paste(getwd(), "/Scripts/GetOutputs.R", sep = ''))
 source(paste(getwd(), "/Scripts/InitializeSounders.R", sep = ''))
 source(paste(getwd(), "/Scripts/FastMovement.R", sep = ''))
 source(paste(getwd(), "/Scripts/SimulateOneRun.R", sep = ''))
+source(paste(getwd(), "/Scripts/StateChanges.R", sep = ''))
 source(paste(getwd(), "/Scripts/FOI.R", sep = ''))
 
 #cpp Functions
