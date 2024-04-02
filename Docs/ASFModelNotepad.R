f239b18154aaba1ddd3c4b76652eed035d8692e1
kchalkowski <kzc0061@auburn.edu>
2024-04-02 09:29:03 -0600

    removed unused/old scripts from Scripts folder, left only scripts accessed by ASFFunctionSourcer

--- Docs/ASFModelNotepad.R
+++
@@ -1,4 +0,0 @@
-##Current troubleshooting needed
-#issue with chull function
-#initialize sounders creates rows with 0 sounders
-#need to add if statement to stop dead pigs from moving
--- Scripts/RunASFModel.R
+++
@@ -1,1 +0,0 @@
-SimulateOneRun(Pcr,Pir,Pbd,death,F1,F2,F2i,B1,B2,thyme,cells,N0,K,detectday,Rad,Intensity,alphaC,shift,centroids,cullstyle,inc,ss,gridlen,midpoint,pop)
