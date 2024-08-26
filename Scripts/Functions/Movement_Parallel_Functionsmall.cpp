44b9b9f7a822b1752ef4669301c3afb913095d05
kchalkowski <kzc0061@auburn.edu>
2024-08-26 06:55:42 -0600

    added more comments to rcpp movement function

--- Scripts/Functions/Movement_Parallel_Functionsmall.cpp
+++ Scripts/Functions/Movement_Parallel_Functionsmall.cpp
@@ -14,42 +14,32 @@ using namespace arma;
 
 //this wrapper makes the function available to RcppParallel sort of
 //the function that you want made parallel goes here in void operator
-//struct SquareRoot : public Worker
 struct MoveLoop : public Worker {
-   // source matrix
-   //const RMatrix<double> input;
+   // input matrices
    const RMatrix<double> apop;
    const RMatrix<int> apopabund;
    const RMatrix<int> apoplocs;
    const RMatrix<double> acent;
 
-   // destination matrix
-   //RMatrix<double> output;
+   // output matrix
    RMatrix<double> outpop;
 
-   // initialize with source and destination
+   // initialize with input and output
    //kind of like calling the function with a header, but with slightly different format
-   //SquareRoot(const NumericMatrix input, NumericMatrix output) 
-   //  : input(input), output(output) {}
 
-  MoveLoop(const NumericMatrix& apop,
+
+
+MoveLoop(const NumericMatrix& apop,
            const IntegerMatrix& apopabund,
            const IntegerMatrix& apoplocs,
            const NumericMatrix& acent,
            NumericMatrix outpop) 
            : apop(apop), apopabund(apopabund), apoplocs(apoplocs), acent(acent), outpop(outpop) {}
 
-   // take the square root of the range of elements requested
-   //this is where the function goes
-   //need to use format for initialization-- void operator()(input stuff like in function) {function goes here}
-   //tells it to go through the specified range size_t begin and end...
-   //void operator()(std::size_t begin, std::size_t end) {
-   //   std::transform(input.begin() + begin, 
-   //                  input.begin() + end, 
-   //                  output.begin() + begin, 
-   //                  ::sqrt);
-   //}
-
+//Below conversion funcs are in place because we need them read in as NumericMatrix/IntegerMatrix format
+//this is native format for Rcpp, and plays well with RcppParallel
+//However, arma formats speeds up simulations considerably
+//Reading them in as NumericMatrix/IntegerMatrix and converting them made it work
 arma::mat convertpop()
  {
       RMatrix<double> tmp_mat = apop;
@@ -82,6 +72,7 @@ arma::imat convertapoplocs()
 ///////Movement function begins here//////
 /////////////////////////////////////////
 
+
 void operator()(std::size_t begin, std::size_t end) {
     // rows we will operate on
         arma::mat apop3 = convertpop();
@@ -89,12 +80,10 @@ void operator()(std::size_t begin, std::size_t end) {
         arma::imat apopabund3 = convertapopabund();
         arma::imat apoplocs3 = convertapoplocs();
         arma::mat diff(acent.nrow(),1);
-        //arma::uvec truemin;
-
 
 ///////Starting loop through piggy pop matrix//////
 
-//for( j = 0; j < np; ++j) {
+//loop through j rows of pop matrix
 for(std::size_t j = begin; j < end; j++) {
 
 //get pointers for centroids matrix
@@ -105,12 +94,12 @@ double* cent_y = acent3.colptr(1);
 
 //initialize integers for pig movement distance and abundance
 //slightly faster than grabbing the numbers each time
-double pop_j_3=apop3(j,3); //distance pop[,4]
-int pop_j_0=apop3(j,0); //abundance pop[,1]
+double pop_j_3=apop3(j,3); //distancem pop[,4]
+int pop_j_0=apop3(j,0); //abundance, pop[,1]
 
 //loop through each element in centroids to get distance, then take the difference
 
-//if abundance and distance greater than zero
+//if abundance (pop_j_0) and distance (pop_j_3) are greater than zero
 if(pop_j_3 > 0 & pop_j_0 > 0){
 
 //initialize distance matrix mask...
@@ -120,11 +109,14 @@ double diffk_0; //initialize diffk_0 double
 
 //loop through each cell in centroids (acent3)
 for(std::size_t k = 0; k < acent.nrow(); k++) {
+
 //get distance between current sounder and each cell in centroids, using spatial distance function
-//get difference between assigned movement distance, and calculated distance between present location and each cell
-//shouldn't this be absolute value? otherwise can have negative and won't be picked up by diffk mask
+//get difference between that and assigned movement distance
 diffk_0=abs(sqrt(pow((cent_x[k]-apop3(j,4)),2)+pow((cent_y[k]-apop3(j,5)),2))-apop3(j,3));
-diff(k,0)=diffk_0; //assign distance to distance matrix
+
+//assign difference between assigned and actual distance to diff matrix
+diff(k,0)=diffk_0;
+
 //find diffk_0 closest to assigned movement distance, set mask to isolate those
 if(diffk_0>=0 & diffk_0<=0.4){
 mask[k]=1;
@@ -134,32 +126,39 @@ mask[k]=0;
 
 } //going through centroids closing bracket
 
-//get the indices for set of locations with distance nearest to the assigned movement distance
+//get the indices for set of locations with distance near the assigned movement distance
 arma::uvec set = find(mask==1);
-//Rcout << "set" << set << "\n";
+
 //get size of set
 const int setsize = set.n_elem;
-
 
 ///////Decide which cells in set to move to, based on abundance//////
 
 //if some possible cells to move to...
 if(setsize>0){
+
 //initialize vector for total abundance in each cell in set
-arma::ivec abund(setsize); 
+arma::ivec abund(setsize);
+
+//make mask vector 'setmask' for grabbing indices of set in loop below
 arma::ivec setmask(apoplocs3.n_rows);
+
 //loop through each cell in set, get total abundance in each cell in set
 for(std::size_t s = 0; s < setsize; ++s) {
-int set_s = set(s); //get index of current cell in set
+
+//get index of current cell in set
+int set_s = set(s);
+
 //run through each item in apoplocs, get locations of each cell in set
 //need to do this before get abundances, because there is possibility that
 //multiple sounders could be in a single cell
 //this means that can't just find cell with min abundance first and then get that location
-//need to find all sounders in each cell in set, sum abundance
+//need to find all sounders in each cell in set, then sum abundance
 for(std::size_t p = 0; p < apoplocs3.n_rows; ++p){
-if(apoplocs3(p)==set_s) setmask[s]=1; //set poplocs mask to 1 if any loc matches cell in set
+
+//set poplocs mask to 1 if any loc matches cell in set
+if(apoplocs3(p)==set_s) setmask[s]=1; 
 else setmask[s]=0;
-}
 
 //get abundances of cells in set
 imat abundinset_s = apopabund3.rows(find(setmask==1));
@@ -171,61 +170,52 @@ abund(s)=sum(abundinset_s.col(0));
 //get the index of cell(s) in set equal to the minimum value
 //this is subset of set, so the index is actually of centroids.. aka cell ID
 
-////////////////////////////////////////
-///Test block
-
 //get minimum abundance value
 int minabundinset = abund.min();
 arma::ivec abundmask(abund.n_rows);
 
 //Return indices of set which are equal to the minimum abundance value
 for(std::size_t p = 0; p < abund.n_rows; ++p){
-  if(abund(p)==minabundinset) abundmask[p]=1; //set poplocs mask to 1 if any loc matches cell in set
+//set poplocs mask to 1 if any loc matches cell in set
+  if(abund(p)==minabundinset) abundmask[p]=1; 
   else abundmask[p]=0;
 }
 
+//cellindarma=cell index, arma format
 //then, cellindarma shouldbe wherever mask==1 (ie, wherever the value was equal to the minimum)
-//imat abundinset_s = apopabund3.rows(find(setmask==1));
 arma::uvec cellindarma = set.elem(find(abundmask==1));
 
-////////////////////////////////////////
-
-
-//arma::uvec cellindarma=set.row(abund.index_min());
-
-//Rcout << "cellindarma " << cellindarma << "\n";
-arma::uvec truemin; //initialize selected minimum value vector
+//initialize selected minimum value vector
+arma::uvec truemin; 
 
 //if there are more than 1 cell with same minimum abundance, choose one at random
 if(cellindarma.size()>1){
 truemin = Rcpp::RcppArmadillo::sample(cellindarma,1,false);
 
 } else {
+//else, truemin is just the one with the min abundance
 truemin = cellindarma;
 }
-
-//Rcout << "truemin " << truemin << "\n";
 
 ///////Assign location for sounder//////
 
 //set location to selected cell in set with minimum abundance
 outpop(j,0)=truemin[0]+1; //+1 is to get appropriate index
 
-} else{ //this is the else to the if statement 'if any cells in set'
-
-//there should always be a possible cell to move to (unless barriers introduced in model)
-//currently, if no cells in set, should generate error
+} else{ 
+//this is the else to the if statement 'if any cells in set'
+//there should always be a possible cell to move to unless barriers introduced in model
+//for now, choosing to generate error bc means something weird going on
 //this will output unrealistic location number that will be used in R script to generate error
 outpop(j,0)=acent.nrow()+1000;
-
 
 }
 
 
-} else{ //if movement distance is zero, or abundance is zero
-
+} else{
+//this is else to 'if movement distance is zero, or abundance is zero'
+//if so, destination location is same as original location
 outpop(j,0)=apoplocs(j,0);
-
 
 }
 
@@ -256,20 +246,16 @@ NumericMatrix parallelMovementRcpp_portion(const NumericMatrix& apop,
   //essentially just defining the size of the output
   NumericMatrix outpop(apop.nrow(), 1);
   
-  // SquareRoot functor (pass input and output matrixes)
-  // x is input matrix defined above
-  //output is output matrix defined above
-  //SquareRoot squareRoot(x, output);
+  //output is output matrix defined early in script
     MoveLoop moveloop(apop,apopabund,apoplocs,acent,outpop);
-   //apop(apop), apopabund(apopabund), apoplocs(apoplocs), acent(acent), cells(cells), np(np), popoutput(popoutput) {}
+  
   // call parallelFor to do the work
-  // starting from 0 to length of x, run the squareRoot function defined above in the worker
-  //parallelFor(0, x.length(), squareRoot);
+  // starting from 0 to length of apop.nrow(), run the squareRoot function defined above in the worker
     parallelFor(0,apop.nrow(), moveloop);
   
   // return the output matrix
   return outpop;
-//}
+
 }
 
 ///////////////////////////
@@ -288,8 +274,6 @@ Rcpp::NumericMatrix popout = apop;
 popout(_,6)=popout(_,2);
 
 popout(_,2)=parallelMovementRcpp_portion(apop,apopabund,apoplocs,acent);
-//popout(_,4)=acent(popout(_,2),0);
-//popout(_,5)=acent(popout(_,2),1);
 
 return popout;
 
