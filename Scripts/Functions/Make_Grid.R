e9da6b6a2156b23fd0b3665b6890bee5a19708e6
kchalkowski <kzc0061@auburn.edu>
2024-08-26 09:54:41 -0600

    Fixed incorrect centroid setup

--- Scripts/Functions/Make_Grid.R
+++ Scripts/Functions/Make_Grid.R
@@ -16,7 +16,6 @@
 #0.4*200
 
 #Note: stuck on running clustered simulated landscape with NLMR due to pkg install issues
-#Sent ticket to IT on Apr 9, follow up if no resolution in a week
 # install.packages("remotes")
 #remotes::install_github("cran/RandomFieldsUtils")
 #remotes::install_github("cran/RandomFields")
@@ -24,35 +23,65 @@
 
 #require(NLMR)
 #Make_Grid(100,0.4,"homogenous")
+#len=100
+#inc=0.4
+#grid.opt="homogenous"
+###
+#troubleshooting:
+#grid<-readMat(paste0(home,"/Input/Grid_80x80_0pt4km.mat"))
+#gridml<-grid$grid
+#centroidsml=gridml[,6:7]
+###
 
-Make_Grid<-function(area,inc,grid.opt){
-  cells=(area/(inc^2))
-  nrow.grid=area/inc
+Make_Grid<-function(len,inc,grid.opt){
+  
+  #get number of cells in grid
+  cells=len^2
+  
+  #if grid homogenous-- will later enter ability to alter LULC
   if(grid.opt=="homogenous"){
+  
+  #initialize empty grid matrix
   grid=matrix(nrow=round(cells),ncol=7)
+  
+  #first column is just cell indices
   grid[,1]=1:cells
-  grid[,2]=rep(seq(0,((inc*nrow.grid)-inc),inc),nrow.grid)
-  grid[,3]=rep(seq(0,((inc*nrow.grid)-inc),inc),nrow.grid)
-  grid[,4]=rep(seq(inc,(inc*nrow.grid),inc),nrow.grid)
-  grid[,5]=rep(seq(inc,(inc*nrow.grid),inc),nrow.grid)
-  grid[,6]=rep(seq(((0+inc)/2),(((inc*nrow.grid)-inc)+(inc*nrow.grid))/2,inc),nrow.grid)
-  grid[,7]=rep(seq(((0+inc)/2),(((inc*nrow.grid)-inc)+(inc*nrow.grid))/2,inc),nrow.grid)
+  
+  #Top left X coordinate of each cell
+  grid[,2]=rep(seq(0,((inc*len)-inc),inc),times=len)
+  
+  #Top left Y coordinate of each cell
+  grid[,3]=rep(seq(0,((inc*len)-inc),inc),each=len)
+  
+  #Top right X coordinate of each cell
+  grid[,4]=rep(seq(inc,(inc*len),inc),times=len)
+  
+  #Top right Y coordinate of each cell
+  grid[,5]=rep(seq(inc,(inc*len),inc),each=len)
+  
+  #Center X coordinate of each cell
+  grid[,6]=rep(seq(((0+inc)/2),(((inc*len)-inc)+(inc*len))/2,inc),times=len)
+ 
+  #Center Y coordinate of each cell
+  grid[,7]=rep(seq(((0+inc)/2),(((inc*len)-inc)+(inc*len))/2,inc),each=len)
+  
+  #get centroids-only object
   centroids=grid[,c(6,7)]
   }
 
   
-  if(grid.opt=="test_pref_binary"){
-    grid=matrix(nrow=round(cells),ncol=8)
-    grid[,1]=1:cells
-    grid[,2]=rep(seq(0,((inc*nrow.grid)-inc),0.4),nrow.grid)
-    grid[,3]=rep(seq(0,((inc*nrow.grid)-inc),0.4),nrow.grid)
-    grid[,4]=rep(seq(inc,(inc*nrow.grid),0.4),nrow.grid)
-    grid[,5]=rep(seq(inc,(inc*nrow.grid),0.4),nrow.grid)
-    grid[,6]=rep(seq(((0+inc)/2),(((inc*nrow.grid)-inc)+(inc*nrow.grid))/2,0.4),nrow.grid)
-    grid[,7]=rep(seq(((0+inc)/2),(((inc*nrow.grid)-inc)+(inc*nrow.grid))/2,0.4),nrow.grid)
-    grid[,8]=rbinom(round(cells),1,0.2)
-    centroids=grid[,c(6,7)]
-  }
+  #if(grid.opt=="test_pref_binary"){
+  #  grid=matrix(nrow=round(cells),ncol=8)
+  #  grid[,1]=1:cells
+  #  grid[,2]=rep(seq(0,((inc*nrow.grid)-inc),0.4),nrow.grid)
+  #  grid[,3]=rep(seq(0,((inc*nrow.grid)-inc),0.4),nrow.grid)
+  #  grid[,4]=rep(seq(inc,(inc*nrow.grid),0.4),nrow.grid)
+  #  grid[,5]=rep(seq(inc,(inc*nrow.grid),0.4),nrow.grid)
+  #  grid[,6]=rep(seq(((0+inc)/2),(((inc*nrow.grid)-inc)+(inc*nrow.grid))/2,0.4),nrow.grid)
+  #  grid[,7]=rep(seq(((0+inc)/2),(((inc*nrow.grid)-inc)+(inc*nrow.grid))/2,0.4),nrow.grid)
+  #  grid[,8]=rbinom(round(cells),1,0.2)
+  #  centroids=grid[,c(6,7)]
+  #}
   
   grid.list=list("cells"=cells,"grid"=grid,"centroids"=centroids)
   return(grid.list)
