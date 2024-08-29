98b3cde8750da2453790fb8eaf88df67e9285f0d
kchalkowski <kzc0061@auburn.edu>
2024-08-29 06:48:17 -0600

    cleaned up comments on new FOI Rcpp function

--- Scripts/Functions/Fast_FOI_Matrix.cpp
+++ Scripts/Functions/Fast_FOI_Matrix.cpp
@@ -20,65 +20,53 @@ arma::mat Fast_FOI_function(arma::uvec id,
 ) {
 
 
-///const arma::mat& Imat,
-///                            const arma::mat& Cmat,
-///                            const double B1,
-///                            const double B2
-
-
-
-//const arma::mat& M
-
-//initiate output vector
-//arma::mat dist(cells,1,fill::zeros);
-
 const int Ninf = id.n_elem;
 
-//arma::mat x_inf = centroids.submat(id,0);
+//get col centroids
 arma::mat X_centroids = centroids.col(0);
 arma::mat Y_centroids = centroids.col(1);
 
+//get submatrix with locs of infected individuals
 arma::mat Xinf = X_centroids.rows(id);
 arma::mat Yinf = Y_centroids.rows(id);
 
-//centroids.submat(id,0);
+//format matrices for matrix multiplication
 arma::mat X1 = repmat(Xinf, 1, cells);
 arma::mat Y1 = repmat(Yinf, 1, cells);
 
+//format matrices for matrix multiplication
 arma::mat X2 = repmat(trans(X_centroids), Ninf, 1);
 arma::mat Y2 = repmat(trans(Y_centroids), Ninf, 1);
 
+//piecemeal pythagorean distance formula
+//get diffs in x/y locations
 arma::mat diffX = X2-X1;
 arma::mat diffY = Y2-Y1;
 
+//square diffs
 arma::mat diffX_2 = diffX % diffX;
 arma::mat diffY_2 = diffY % diffY;
 
+//sqrt added squared diffs
 arma::mat dist = sqrt(diffX_2+diffY_2);
 
-//arma::mat dist = sqrt((X2-X1)^2+(Y2-Y1)^2);
-
+//use distances in distanct/contact formula to get prob of contact
+//infected live individuals
 arma::mat plogit = F2_int + (F2_B*dist);
 arma::mat prob = exp(plogit)/(1+exp(plogit));
-
+//infected carcasses
 arma::mat pilogit = F2i_int + (F2i_B*dist);
 arma::mat probi = exp(pilogit)/(1+exp(pilogit));
 
 //find where dist=0, set those indices in prob/probi to zero
+//zero distance contact (same cell) is dealt with outside of Rcpp with simpler probability
 arma::uvec dist_zero = find(dist == 0);
-//arma::ivec distmask(dist.n_rows);
-
 prob(dist_zero) = dist(dist_zero);
 probi(dist_zero) = dist(dist_zero);
 
-
-//B=B1*t(repmat(I[id],cells,1))*prob+B2*t(repmat(C[id],cells,1))*probi
+//Get scaled probabilities, combined live infected contact with carcass infected contact
+//B1 and B2 are scaling parameters, determined via sensitivity analyses
 arma::mat B = B1*(Imat%prob)+B2*(Cmat%probi);
-
-//return B;                        
-
-//return Rcpp::List::create(Rcpp::Named("outer")=prob,
-//                              Rcpp::Named("inner")=probi);
 
 return(B);
 
