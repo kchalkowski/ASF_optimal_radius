cf42b633c384ead991947ef0ab87050ca2457249
kchalkowski <kzc0061@auburn.edu>
2024-09-04 12:23:50 -0600

    Added additional data summaries for sensitivity analysis input, fixed bug in previously created summaries, and added functionality for separate carcass/live external apparent prevalence function. Added more processing lines to doSensitivityAnalysis.R.

--- Sensitivity_Analysis/Scripts/ExtApparentPrev.R
+++ Sensitivity_Analysis/Scripts/ExtApparentPrev.R
@@ -6,14 +6,24 @@ ExtApparentPrev<-function(SEIRCZ.rep,DetP,N,rep){
   require(plyr)
   #convert each timestep (week) into rep lists
   for(r in 1:nrow(SEIRCZ.rep)){
-  S<-rep("S",SEIRCZ.rep[r,2]) 
-  E<-rep("S",SEIRCZ.rep[r,3]) #Would E have antibodies?
-  I<-rep("I",SEIRCZ.rep[r,4]) 
-  R<-rep("S",SEIRCZ.rep[r,5]) #Would R have antibodies?
+    #print(1)
+  S<-rep("S",SEIRCZ.rep[r,which(colnames(SEIRCZ.rep)=="S")]) 
+  E<-rep("S",SEIRCZ.rep[r,which(colnames(SEIRCZ.rep)=="E")]) #Would E have antibodies? assign as S for now
+  I<-rep("I",SEIRCZ.rep[r,which(colnames(SEIRCZ.rep)=="I")]) 
+  R<-rep("S",SEIRCZ.rep[r,which(colnames(SEIRCZ.rep)=="R")]) #Would R have antibodies? assign as S for now
+  C<-rep("C",SEIRCZ.rep[r,which(colnames(SEIRCZ.rep)=="C")])
+  Z<-rep("Z",SEIRCZ.rep[r,which(colnames(SEIRCZ.rep)=="Z")])
+  #print(1)
   allpig=c(S,E,I,R)
   samples=sample(allpig,min(N,length(allpig)))
   counts.r=plyr::count(samples)
   counts.r$appar<-0
+  #print(1)
+  allcarcass=c(C,Z)
+  csamples=sample(allcarcass,min(N,length(allcarcass)))
+  counts.c=plyr::count(csamples)
+  #counts.c$appar<-0
+  #print(1)
   if(length(counts.r[counts.r$x=="I",]$freq)>0){
   num.appar.inf<-sum(rbinom(c(1,0),counts.r[counts.r$x=="I",]$freq,prob=c(DetP,(1-DetP))))
   num.add.sus=counts.r[counts.r$x=="I",]$freq-num.appar.inf
@@ -22,11 +32,32 @@ ExtApparentPrev<-function(SEIRCZ.rep,DetP,N,rep){
   } else{
     counts.r$appar=counts.r$freq
   }
-  counts.r$timestep=r
-  if(r==1){
-    counts=counts.r
+  #print(1)
+  if(nrow(counts.c)>0){
+  if(length(counts.c[counts.c$x=="C",]$freq)>0){
+    counts.c$appar=NA
+    num.appar.inf<-sum(rbinom(c(1,0),counts.c[counts.c$x=="C",]$freq,prob=c(DetP,(1-DetP))))
+    num.add.sus=counts.c[counts.c$x=="C",]$freq-num.appar.inf
+    counts.c[counts.c$x=="Z",]$appar=counts.c[counts.c$x=="Z",]$freq+num.add.sus
+    counts.c[counts.c$x=="C",]$appar=num.appar.inf
   } else{
-    counts=rbind(counts,counts.r)
+    counts.c$appar=counts.c$freq
+  }
+  
+  } else{
+    counts.c=data.frame(x="Z",freq=0,appar=0)
+  }
+  
+  counts.r$src="alive"
+  counts.c$src="carcass"
+  
+  counts.rep=rbind(counts.r,counts.c)
+  counts.rep$week=r
+  #print(1)
+  if(r==1){
+    counts=counts.rep
+  } else{
+    counts=rbind(counts,counts.rep)
   }
   }
 
@@ -34,11 +65,32 @@ ExtApparentPrev<-function(SEIRCZ.rep,DetP,N,rep){
   
   counts=pivot_wider(counts,names_from=x,values_from=c(freq,appar)) %>% as.data.frame
   counts[is.na(counts)]<-0
-  counts$freq_prev=counts$freq_I/(counts$freq_I+counts$freq_S)
-  counts$appar_prev=counts$appar_I/(counts$appar_I+counts$appar_S)
+  
+  counts$freq_prev<-0
+  counts[counts$src=="alive",]$freq_prev=counts[counts$src=="alive",]$freq_I/(counts[counts$src=="alive",]$freq_I+counts[counts$src=="alive",]$freq_S)
+  counts[counts$src=="carcass",]$freq_prev=counts[counts$src=="carcass",]$freq_C/(counts[counts$src=="carcass",]$freq_C+counts[counts$src=="carcass",]$freq_Z)
+  counts[is.na(counts)]<-0
+  
+  
+  counts$appar_prev<-0
+  counts[counts$src=="alive",]$appar_prev=counts[counts$src=="alive",]$appar_I/(counts[counts$src=="alive",]$appar_I+counts[counts$src=="alive",]$appar_S)
+  counts[counts$src=="carcass",]$appar_prev=counts[counts$src=="carcass",]$appar_C/(counts[counts$src=="carcass",]$appar_C+counts[counts$src=="carcass",]$appar_Z)
   
   #move rep to front
-  counts=counts[,c(2,1,3:8)]
+  counts=counts[,c(1,3,2,4:13)]
   
-  return(counts)
+  #reorganize to fit outbreak data better
+  counts.alive=counts[counts$src=="alive",]
+  counts.carcass=counts[counts$src=="carcass",]
+  
+  counts.alive=counts.alive[,c(1:4,7,8,11,12,13)]
+  counts.carcass=counts.carcass[,c(1:3,5,6,9,10,12,13)]
+  
+  colnames(counts.alive)[c(4:7)]<-c("freq_Nneg","freq_Npos","appar_Nneg","appar_Npos")
+  colnames(counts.carcass)[c(4:7)]<-c("freq_Nneg","freq_Npos","appar_Nneg","appar_Npos")
+  
+  allcounts=rbind(counts.alive,counts.carcass)
+  allcounts[is.na(allcounts)]<-0
+  
+  return(allcounts)
 }
--- Sensitivity_Analysis/Scripts/doSensitivityAnalysis.R
+++ Sensitivity_Analysis/Scripts/doSensitivityAnalysis.R
@@ -3,7 +3,7 @@
 ########## Purpose ##########
 #############################
 
-#The purpose of this script is to run replicates of ASF simulation model
+#The purpose of this script is to run replicates of ASF simulation model, and match output to ASF outbreak surveillance data.
 
 #########################################
 ########## Set output location ##########
@@ -26,8 +26,11 @@ source(paste0(home,"/Scripts/Setup/SetParameters.R"))
 #loads all functions, variables needed, etc.
 source(paste0(home,"/Scripts/Setup/InitializeASFModel.R"))
 
+#load additional sens. analysis function
+source(paste0(home,"/Sensitivity_Analysis/Scripts/ExtApparentPrev.R"))
+
 #set number of replicates to run
-nrep=100
+nrep=3
 
 #set name of run folder and create folder for run output
 run_folder=gsub("\\..*", "", gsub("-|:| ","_",paste0("TestRun_",Sys.time())),perl=TRUE)
@@ -45,7 +48,7 @@ N=100
 #####################################
 ########## Loop replicates ##########
 #####################################
-rep=1
+#rep=1
 #run loop
 for(rep in 1:nrep){
   
@@ -69,3 +72,69 @@ for(rep in 1:nrep){
       SI_apparent.summary=rbind(SI_apparent.summary,SI_apparent.rep)
     }
 }
+
+
+#save output
+write.csv(SEIRCZ.summary,paste0("Sensitivity_Analysis/Output/",run_folder,"/SEIRCZ.summary.csv"),row.names=FALSE)
+write.csv(SI_apparent.summary,paste0("Sensitivity_Analysis/Output/",run_folder,"/SI_apparent.summary.csv"),row.names=FALSE)
+
+####################################################################
+########## Match format to ASF outbreak surveillance data ##########
+####################################################################
+#SI_apparent.summary=read.csv(paste0("Sensitivity_Analysis/Output/",run_folder,"/SI_apparent.summary.csv"))
+
+#summarize SI_apparent.summary across replicates
+SI_apparent.summary[is.na(SI_apparent.summary)]<-0
+SI_apparent.summary=SI_apparent.summary %>% dplyr::group_by(src,week) %>% dplyr::summarise(freq_prev=mean(freq_prev),freq_prev_q25=quantile(freq_prev,0.25),freq_prev_q75=quantile(freq_prev,0.75),appar_prev=mean(appar_prev),appar_prev_q25=quantile(appar_prev,0.25),appar_prev_q75=quantile(appar_prev,0.75))
+
+#read ASF outbreak data
+edat=readRDS(paste0("Sensitivity_Analysis/Input/datE80km_weekly.rds"))
+wdat=readRDS(paste0("Sensitivity_Analysis/Input/datW80km_weekly.rds"))
+
+edat2=edat[,c(1,2,3,9)]
+wdat2=wdat[,c(1,2,3,9)]
+
+#rename cols to match
+colnames(edat2)[4]<-"appar_prev"
+colnames(wdat2)[4]<-"appar_prev"
+#colnames(SI_apparent.summary)[2]<-"week"
+
+add_vars <- function(df,vars,val){
+  df[vars] <- val
+  df
+}
+
+edat2$type="observed_east"
+wdat2$type="observed_west"
+SI_apparent.summary$type="simulated"
+
+new_vars=colnames(SI_apparent.summary)[!(colnames(SI_apparent.summary)%in%colnames(edat2))]
+
+edat2=edat2 |>
+  add_vars(df = _,vars = new_vars,val = NA)
+wdat2=wdat2 |>
+  add_vars(df = _,vars = new_vars,val = NA)
+
+new_vars=colnames(edat2)[!(colnames(edat2)%in%colnames(SI_apparent.summary))]
+SI_apparent.summary=SI_apparent.summary |>
+  add_vars(df = _,vars = new_vars,val = NA)
+
+l<-list(observed_east=edat2,
+        observed_west=wdat2,
+        simulated=SI_apparent.summary)
+
+sadf=do.call(rbind, lapply(l, function(x) x[match(names(l[[1]]), names(x))]))
+
+################################
+########## Make plots ##########
+################################
+
+library(ggplot2)
+live=sadf[sadf$src=="alive"|sadf$src=="live",]
+dead=sadf[sadf$src=="carcass",]
+ggplot(live, aes(x=week,y=appar_prev,color=type))+geom_line()
+ggplot(dead, aes(x=week,y=appar_prev,color=type))+geom_line()
+
+
+
+
